05224274371b7724d033c77abdff0ba1bf11d8fa
Aaron Crane <arc@cpan.org>
2012-05-05 14:26:39 +0100

    examples/t.pl: accept optional filename argument

--- examples/t.pl
+++ examples/t.pl
@@ -4,7 +4,7 @@ use strict;
 use Image::EXIF;
 use Data::Dumper;
 
-my $exif = new Image::EXIF("i424f2d1c0.jpg");
+my $exif = new Image::EXIF($ARGV[0] || "i424f2d1c0.jpg");
 
 my $all_info = $exif->get_all_info();
 
